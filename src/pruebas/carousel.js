3a1b35f7f7c673cd41c299b150fd57b69ff18d94
david <david_cd97@hotmail.com>
2023-02-17 18:35:08 +0100

    Añadido: animación background

--- src/pruebas/carousel.js
+++ src/pruebas/carousel.js
@@ -1,32 +1,8 @@
-chars = [
-  "園",
-  "迎",
-  "簡",
-  "益",
-  "大",
-  "诶",
-  "比",
-  "西",
-  "迪",
-  "伊",
-  "弗",
-  "吉",
-  "尺",
-  ,
-  "杰",
-  "开",
-  "艾",
-  "勒",
-  "马",
-  "娜",
-];
-
-
 const matrixCharacters = document.querySelectorAll('.matrix-character');
 
 matrixCharacters.forEach(character => {
   // Genera un valor aleatorio entre 0.5 y 2 para la velocidad de la animación
-  const speed = Math.random() * 2 + 0.5;
+  const speed = Math.random() * 1.5 + 0.5;
   character.style.animationDuration = speed + 's';
 
   // Genera un valor aleatorio entre 10px y 40px para el tamaño del caracter
