6a11d0a09152c7fd37cfdf295691bb79df21d9f6
david <david_cd97@hotmail.com>
2023-02-27 20:59:04 +0100

    Tarjetas autogeneradas por JS Página del curso genérica autogenerada por JS

---
+++ src/js/cursos.js
@@ -0,0 +1,293 @@
+// todos los cursos en un json
+
+const cursos = [
+  {
+    id: 1,
+    nombre: "Curso de NodeJS",
+    duracion: "32 horas",
+    valor: 29.99,
+    imagen:
+      "https://4.bp.blogspot.com/-wPEafonQ2OY/VS4vAbqoZzI/AAAAAAAAJZk/q8H7Hi29Z3Q/s1600/jtr.png",
+    introduccion: "",
+    introduccion:
+      "Bienvenidos al curso de Hydra, una herramienta muy útil para la" +
+      "realización de ataques de fuerza bruta en sistemas protegidos con" +
+      "contraseñas. Durante el curso, aprenderemos a utilizar Hydra en" +
+      "combinación con Kali Linux.",
+    temario: [
+      "Introducción a Hydra y Kali Linux",
+      "Instalación de Hydra y Kali Linux",
+      "Configuración de Hydra",
+      "Realización de ataques de fuerza bruta con Hydra",
+      "Protección contra ataques de fuerza bruta",
+    ],
+
+    requisitos:
+      "Para tomar este curso, es necesario tener conocimientos " +
+      "básicos de Linux y redes de computadoras.",
+
+    profesor: "Andrés Pantoja",
+
+    video: {
+      src: "img/49p0tf.mp4",
+      type: "video/mp4",
+    },
+  },
+  {
+    id: 2,
+    nombre: "Curso de Nmap",
+    duracion: "48 horas",
+    valor: 18.98,
+    imagen: "https://mytcpip.com/wp-content/uploads/2015/09/nmap.jpg",
+    introduccion:
+      "Bienvenidos al curso de Hydra, una herramienta muy útil para la" +
+      "realización de ataques de fuerza bruta en sistemas protegidos con" +
+      "contraseñas. Durante el curso, aprenderemos a utilizar Hydra en" +
+      "combinación con Kali Linux.",
+    temario: [
+      "Introducción a Hydra y Kali Linux",
+      "Instalación de Hydra y Kali Linux",
+      "Configuración de Hydra",
+      "Realización de ataques de fuerza bruta con Hydra",
+      "Protección contra ataques de fuerza bruta",
+    ],
+
+    requisitos:
+      "Para tomar este curso, es necesario tener conocimientos " +
+      "básicos de Linux y redes de computadoras.",
+
+    profesor: "Andrés Pantoja",
+
+    video: {
+      src: "img/49p0tf.mp4",
+      type: "video/mp4",
+    },
+  },
+  {
+    id: 3,
+    nombre: "Curso de Hydra",
+    duracion: "64 horas",
+    valor: 27.89,
+    imagen: "https://spyboyblog.files.wordpress.com/2020/12/hydrimage.jpg",
+    introduccion:
+      "Bienvenidos al curso de Hydra, una herramienta muy útil para la" +
+      "realización de ataques de fuerza bruta en sistemas protegidos con" +
+      "contraseñas. Durante el curso, aprenderemos a utilizar Hydra en" +
+      "combinación con Kali Linux.",
+    temario: [
+      "Introducción a Hydra y Kali Linux",
+      "Instalación de Hydra y Kali Linux",
+      "Configuración de Hydra",
+      "Realización de ataques de fuerza bruta con Hydra",
+      "Protección contra ataques de fuerza bruta",
+    ],
+
+    requisitos:
+      "Para tomar este curso, es necesario tener conocimientos " +
+      "básicos de Linux y redes de computadoras.",
+
+    profesor: "Andrés Pantoja",
+
+    video: {
+      src: "img/49p0tf.mp4",
+      type: "video/mp4",
+    },
+  },
+  {
+    id: 4,
+    nombre: "Curso de Kali Linux",
+    duracion: "80 horas",
+    valor: 32.99,
+    imagen: "https://i.ytimg.com/vi/XjjaSmUQm1c/maxresdefault.jpg",
+    introduccion: "",
+    introduccion:
+      "Bienvenidos al curso de Hydra, una herramienta muy útil para la" +
+      "realización de ataques de fuerza bruta en sistemas protegidos con" +
+      "contraseñas. Durante el curso, aprenderemos a utilizar Hydra en" +
+      "combinación con Kali Linux.",
+    temario: [
+      "Introducción a Hydra y Kali Linux",
+      "Instalación de Hydra y Kali Linux",
+      "Configuración de Hydra",
+      "Realización de ataques de fuerza bruta con Hydra",
+      "Protección contra ataques de fuerza bruta",
+    ],
+
+    requisitos:
+      "Para tomar este curso, es necesario tener conocimientos " +
+      "básicos de Linux y redes de computadoras.",
+
+    profesor: "Andrés Pantoja",
+
+    video: {
+      src: "img/49p0tf.mp4",
+      type: "video/mp4",
+    },
+  },
+  {
+    id: 5,
+    nombre: "Curso de Subnettting",
+    duracion: "96 horas",
+    valor: 0,
+    imagen:
+      "https://www.pclocs.com.au/wp-content/uploads/2020/01/subnet-logo-300x300-1.jpg",
+    introduccion: "",
+    introduccion:
+      "Bienvenidos al curso de Hydra, una herramienta muy útil para la" +
+      "realización de ataques de fuerza bruta en sistemas protegidos con" +
+      "contraseñas. Durante el curso, aprenderemos a utilizar Hydra en" +
+      "combinación con Kali Linux.",
+    temario: [
+      "Introducción a Hydra y Kali Linux",
+      "Instalación de Hydra y Kali Linux",
+      "Configuración de Hydra",
+      "Realización de ataques de fuerza bruta con Hydra",
+      "Protección contra ataques de fuerza bruta",
+    ],
+
+    requisitos:
+      "Para tomar este curso, es necesario tener conocimientos " +
+      "básicos de Linux y redes de computadoras.",
+
+    profesor: "Andrés Pantoja",
+
+    video: {
+      src: "img/49p0tf.mp4",
+      type: "video/mp4",
+    },
+  },
+  {
+    id: 6,
+    nombre: "Curso de CSRF Tokens",
+    duracion: "112 horas",
+    valor: 0,
+    imagen: "https://brightsec.com/wp-content/uploads/2021/06/CSRF-tokens.png",
+    introduccion: "",
+    introduccion:
+      "Bienvenidos al curso de Hydra, una herramienta muy útil para la" +
+      "realización de ataques de fuerza bruta en sistemas protegidos con" +
+      "contraseñas. Durante el curso, aprenderemos a utilizar Hydra en" +
+      "combinación con Kali Linux.",
+    temario: [
+      "Introducción a Hydra y Kali Linux",
+      "Instalación de Hydra y Kali Linux",
+      "Configuración de Hydra",
+      "Realización de ataques de fuerza bruta con Hydra",
+      "Protección contra ataques de fuerza bruta",
+    ],
+
+    requisitos:
+      "Para tomar este curso, es necesario tener conocimientos " +
+      "básicos de Linux y redes de computadoras.",
+
+    profesor: "Andrés Pantoja",
+
+    video: {
+      src: "img/49p0tf.mp4",
+      type: "video/mp4",
+    },
+  },
+];
+
+function vistaCurso(curso){
+  const contenedor = document.getElementById("contenedor");
+
+  // Título del curso
+  const titulo = document.createElement("h1");
+  console.log(curso.nombre);
+  titulo.textContent = curso.nombre;
+  contenedor.appendChild(titulo);
+
+  // Imagen del curso
+  const imagen = document.createElement("img");
+  imagen.src = curso.imagen;
+  imagen.alt = curso.nombre;
+  contenedor.appendChild(imagen);
+
+  // Introducción del curso
+  const introTitulo = document.createElement("h2");
+  introTitulo.textContent = "Introducción";
+  contenedor.appendChild(introTitulo);
+
+  const introduccion = document.createElement("p");
+  introduccion.textContent = curso.introduccion;
+  contenedor.appendChild(introduccion);
+
+  // Temario del curso
+  const temaTitulo = document.createElement("h2");
+  introTitulo.textContent = "Introducción";
+  contenedor.appendChild(introTitulo);
+
+  const temarioLista = document.createElement("ul");
+  curso.temario.forEach((tema) => {
+    const temaItem = document.createElement("li");
+    temaItem.textContent = tema;
+    temarioLista.appendChild(temaItem);
+  });
+
+  contenedor.appendChild(temarioLista);
+
+  // Requisitos del curso
+  const requisitosTitulo = document.createElement("h2");
+  requisitosTitulo.textContent = "Requisitos";
+  contenedor.appendChild(requisitosTitulo);
+
+  const requisitos = document.createElement("p");
+  requisitos.textContent = curso.requisitos;
+  contenedor.appendChild(requisitos);
+
+  // Profesor del curso
+  const profesorTitulo = document.createElement("h2");
+  profesorTitulo.textContent = "Profesor";
+  contenedor.appendChild(profesorTitulo);
+
+  const profesor = document.createElement("p");
+  profesor.textContent = curso.profesor;
+  contenedor.appendChild(profesor);
+
+  // Video del curso
+
+  /*
+    <div id="video-container">
+          <video autoplay loop muted>
+            <source id="video" />
+          </video>
+          <div class="overlay">
+            <p>Inscríbete para ver el curso completo!</p>
+          </div>
+        </div>
+    */
+  
+    const videoContainer = document.createElement("div");
+    videoContainer.id = "video-container";
+    contenedor.appendChild(videoContainer);
+
+    const video = document.createElement("video");
+    video.autoplay = true;
+    video.loop = true;
+    video.muted = true;
+    videoContainer.appendChild(video);
+
+    const videoSource = document.createElement("source");
+    videoSource.src = curso.video.src;
+    videoSource.type = curso.video.type;
+    video.appendChild(videoSource);
+
+    const overlay = document.createElement("div");
+    overlay.classList.add("overlay");
+    videoContainer.appendChild(overlay);
+
+    const overlayText = document.createElement("p");
+    overlayText.textContent = "Inscríbete para ver el curso completo!";
+    overlay.appendChild(overlayText);
+    
+};
+
+// Obtener el id del curso del atributo id en la url
+const url = new URLSearchParams(window.location.search);
+
+const id = url.get("id");
+
+const curso = cursos.find((curso) => curso.id == id);
+
+vistaCurso(curso);
---
+++ src/js/generaTarjetas.js
@@ -0,0 +1,252 @@
+// todos los cursos en un json
+
+const cursos = [
+  {
+    id: 1,
+    nombre: "Curso de NodeJS",
+    duracion: "32 horas",
+    valor: 29.99,
+    imagen:
+      "https://4.bp.blogspot.com/-wPEafonQ2OY/VS4vAbqoZzI/AAAAAAAAJZk/q8H7Hi29Z3Q/s1600/jtr.png",
+    introduccion: "",
+    introduccion:
+      "Bienvenidos al curso de Hydra, una herramienta muy útil para la" +
+      "realización de ataques de fuerza bruta en sistemas protegidos con" +
+      "contraseñas. Durante el curso, aprenderemos a utilizar Hydra en" +
+      "combinación con Kali Linux.",
+    temario: [
+      "Introducción a Hydra y Kali Linux",
+      "Instalación de Hydra y Kali Linux",
+      "Configuración de Hydra",
+      "Realización de ataques de fuerza bruta con Hydra",
+      "Protección contra ataques de fuerza bruta",
+    ],
+
+    requisitos:
+      "Para tomar este curso, es necesario tener conocimientos " +
+      "básicos de Linux y redes de computadoras.",
+
+    profesor: "Andrés Pantoja",
+
+    video: {
+      src: "img/49p0tf.mp4",
+      type: "video/mp4",
+    },
+  },
+  {
+    id: 2,
+    nombre: "Curso de Nmap",
+    duracion: "48 horas",
+    valor: 18.98,
+    imagen: "https://mytcpip.com/wp-content/uploads/2015/09/nmap.jpg",
+    introduccion:
+      "Bienvenidos al curso de Hydra, una herramienta muy útil para la" +
+      "realización de ataques de fuerza bruta en sistemas protegidos con" +
+      "contraseñas. Durante el curso, aprenderemos a utilizar Hydra en" +
+      "combinación con Kali Linux.",
+    temario: [
+      "Introducción a Hydra y Kali Linux",
+      "Instalación de Hydra y Kali Linux",
+      "Configuración de Hydra",
+      "Realización de ataques de fuerza bruta con Hydra",
+      "Protección contra ataques de fuerza bruta",
+    ],
+
+    requisitos:
+      "Para tomar este curso, es necesario tener conocimientos " +
+      "básicos de Linux y redes de computadoras.",
+
+    profesor: "Andrés Pantoja",
+
+    video: {
+      src: "img/49p0tf.mp4",
+      type: "video/mp4",
+    },
+  },
+  {
+    id: 3,
+    nombre: "Curso de Hydra",
+    duracion: "64 horas",
+    valor: 27.89,
+    imagen: "https://spyboyblog.files.wordpress.com/2020/12/hydrimage.jpg",
+    introduccion:
+      "Bienvenidos al curso de Hydra, una herramienta muy útil para la" +
+      "realización de ataques de fuerza bruta en sistemas protegidos con" +
+      "contraseñas. Durante el curso, aprenderemos a utilizar Hydra en" +
+      "combinación con Kali Linux.",
+    temario: [
+      "Introducción a Hydra y Kali Linux",
+      "Instalación de Hydra y Kali Linux",
+      "Configuración de Hydra",
+      "Realización de ataques de fuerza bruta con Hydra",
+      "Protección contra ataques de fuerza bruta",
+    ],
+
+    requisitos:
+      "Para tomar este curso, es necesario tener conocimientos " +
+      "básicos de Linux y redes de computadoras.",
+
+    profesor: "Andrés Pantoja",
+
+    video: {
+      src: "img/49p0tf.mp4",
+      type: "video/mp4",
+    },
+  },
+  {
+    id: 4,
+    nombre: "Curso de Kali Linux",
+    duracion: "80 horas",
+    valor: 32.99,
+    imagen: "https://i.ytimg.com/vi/XjjaSmUQm1c/maxresdefault.jpg",
+    introduccion: "",
+    introduccion:
+      "Bienvenidos al curso de Hydra, una herramienta muy útil para la" +
+      "realización de ataques de fuerza bruta en sistemas protegidos con" +
+      "contraseñas. Durante el curso, aprenderemos a utilizar Hydra en" +
+      "combinación con Kali Linux.",
+    temario: [
+      "Introducción a Hydra y Kali Linux",
+      "Instalación de Hydra y Kali Linux",
+      "Configuración de Hydra",
+      "Realización de ataques de fuerza bruta con Hydra",
+      "Protección contra ataques de fuerza bruta",
+    ],
+
+    requisitos:
+      "Para tomar este curso, es necesario tener conocimientos " +
+      "básicos de Linux y redes de computadoras.",
+
+    profesor: "Andrés Pantoja",
+
+    video: {
+      src: "img/49p0tf.mp4",
+      type: "video/mp4",
+    },
+  },
+  {
+    id: 5,
+    nombre: "Curso de Subnettting",
+    duracion: "96 horas",
+    valor: 0,
+    imagen:
+      "https://www.pclocs.com.au/wp-content/uploads/2020/01/subnet-logo-300x300-1.jpg",
+    introduccion: "",
+    introduccion:
+      "Bienvenidos al curso de Hydra, una herramienta muy útil para la" +
+      "realización de ataques de fuerza bruta en sistemas protegidos con" +
+      "contraseñas. Durante el curso, aprenderemos a utilizar Hydra en" +
+      "combinación con Kali Linux.",
+    temario: [
+      "Introducción a Hydra y Kali Linux",
+      "Instalación de Hydra y Kali Linux",
+      "Configuración de Hydra",
+      "Realización de ataques de fuerza bruta con Hydra",
+      "Protección contra ataques de fuerza bruta",
+    ],
+
+    requisitos:
+      "Para tomar este curso, es necesario tener conocimientos " +
+      "básicos de Linux y redes de computadoras.",
+
+    profesor: "Andrés Pantoja",
+
+    video: {
+      src: "img/49p0tf.mp4",
+      type: "video/mp4",
+    },
+  },
+  {
+    id: 6,
+    nombre: "Curso de CSRF Tokens",
+    duracion: "112 horas",
+    valor: 0,
+    imagen: "https://brightsec.com/wp-content/uploads/2021/06/CSRF-tokens.png",
+    introduccion: "",
+    introduccion:
+      "Bienvenidos al curso de Hydra, una herramienta muy útil para la" +
+      "realización de ataques de fuerza bruta en sistemas protegidos con" +
+      "contraseñas. Durante el curso, aprenderemos a utilizar Hydra en" +
+      "combinación con Kali Linux.",
+    temario: [
+      "Introducción a Hydra y Kali Linux",
+      "Instalación de Hydra y Kali Linux",
+      "Configuración de Hydra",
+      "Realización de ataques de fuerza bruta con Hydra",
+      "Protección contra ataques de fuerza bruta",
+    ],
+
+    requisitos:
+      "Para tomar este curso, es necesario tener conocimientos " +
+      "básicos de Linux y redes de computadoras.",
+
+    profesor: "Andrés Pantoja",
+
+    video: {
+      src: "img/49p0tf.mp4",
+      type: "video/mp4",
+    },
+  },
+];
+
+// Estructura de la tarjeta
+/*
+<a href="cursos.html">
+            <div class="card">
+              <img
+                src="https://4.bp.blogspot.com/-wPEafonQ2OY/VS4vAbqoZzI/AAAAAAAAJZk/q8H7Hi29Z3Q/s1600/jtr.png"
+                alt="Imagen 3"
+              />
+              <div class="card_content">
+                <h2>Título de la imagen 3</h2>
+                <p>Descripción de la imagen 3</p>
+                <span>Autor de la imagen 3</span>
+              </div>
+            </div>
+          </a>
+*/
+
+// generar la estructura de la tarjeta con los datos del json
+const generarTarjeta = (curso) => {
+  const tarjeta = document.createElement("a");
+  tarjeta.href = "cursos.html" + "?id=" + curso.id;
+  tarjeta.classList.add("card");
+
+  const imagen = document.createElement("img");
+  imagen.src = curso.imagen;
+  imagen.alt = curso.nombre;
+
+  const card_content = document.createElement("div");
+  card_content.classList.add("card_content");
+
+  const titulo = document.createElement("h2");
+  titulo.textContent = curso.nombre;
+
+  const descripcion = document.createElement("p");
+  descripcion.textContent = curso.duracion;
+
+  const autor = document.createElement("span");
+  autor.textContent = curso.valor;
+
+  card_content.appendChild(titulo);
+  card_content.appendChild(descripcion);
+  card_content.appendChild(autor);
+
+  tarjeta.appendChild(imagen);
+  tarjeta.appendChild(card_content);
+
+  return tarjeta;
+};
+
+// generar las tarjetas
+const generarTarjetas = (cursos) => {
+  const contenedor = document.getElementById("contenedor");
+  cursos.forEach((curso) => {
+    const tarjeta = generarTarjeta(curso);
+    contenedor.appendChild(tarjeta);
+  });
+};
+
+generarTarjetas(cursos);
+
+//
